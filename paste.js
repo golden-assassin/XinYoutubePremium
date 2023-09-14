1088137e6fe41037c81ce5c191d5d705592df7a8
sucks555 <102584033+sucks-555@users.noreply.github.com>
2023-09-15 05:55:39 +0900

    Create paste.js

---
+++ paste.js
@@ -0,0 +1,22 @@
+const player = document.querySelector(".html5-main-video");
+function togglePictureInPicture() {
+  if (document.pictureInPictureElement) {
+    document.exitPictureInPicture();
+  } else {
+    player.requestPictureInPicture();
+  }
+};
+
+const parent = document.querySelector(".ytd-watch-metadata");
+var button = document.createElement('button');
+button.innerHTML = 'picture in picture';
+button.style.cssText = `
+  background-color: rgb(42,42,42);
+  color: white;
+  border-radius: 100vh;
+  border: 0px;
+  padding: 5px;
+  margin: 5px;
+`;
+button.addEventListener('click', togglePictureInPicture);
+parent.appendChild(button);
