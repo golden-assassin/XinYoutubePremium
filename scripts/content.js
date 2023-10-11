eae3850266c246e1f66711931942c3d550667f03
sucks555 <102584033+sucks-555@users.noreply.github.com>
2023-10-11 13:55:58 +0900

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,19 @@
 if (location.host === "www.youtube.com") {
   let video;
   let retry = 0;
-  const maxRetry = 10;
+  const MAX_RETRY = 5;
 
   function handle() {
-    try {
-      const skipButton = document.querySelector(".ytp-ad-skip-button-container, .ytp-ad-overlay-close-container");
-      const adText = document.querySelector(".ytp-ad-text, .ytp-ad-preview-text");
-      if (skipButton || adText) {
-        video.currentTime = 999;
-        if (skipButton) {
-          skipButton.click();
-        }
+    const skipButton = document.querySelector(".ytp-ad-skip-button, .ytp-ad-overlay-close-button");
+    const adText = document.querySelector(".ytp-ad-text, .ytp-ad-preview-text");
+    if (skipButton || adText) {
+      video.currentTime = video.duration;
+      if (skipButton) {
+        skipButton.click();
       }
-    } catch (e) {
-      console.error(`Error while handling ad: ${e.message}`);
     }
   }
+
   function togglePictureInPicture() {
     if (document.pictureInPictureElement) {
       document.exitPictureInPicture();
@@ -24,27 +21,34 @@ if (location.host === "www.youtube.com") {
       video.requestPictureInPicture();
     }
   }
-  function PictureInPictureButton() {
+
+  function addPictureInPictureButton() {
     const controls = document.querySelector(".ytp-right-controls");
     if (controls) {
       const button = document.createElement('button');
-      button.innerHTML = 'Picture';
+      button.innerHTML = 'PiP';
       button.style.cssText = `height:55%;opacity:0.9;display:inline-block;width:48px;padding:0px 2px;overflow:hidden;position:relative;top:-20px;background:transparent;border:1px solid #fff;color:#fff;border-radius:100vh;`;
       button.addEventListener('click', togglePictureInPicture);
       controls.appendChild(button);
-    } else if (retry < maxRetry) {
+    } else if (retry < MAX_RETRY) {
       retry++;
-      setTimeout(PictureInPictureButton, 1000);
+      setTimeout(addPictureInPictureButton, 1000);
     }
   }
-  function waitForPageLoad() {
-    if (document.readyState === "complete") {
-      video = document.querySelector('.html5-main-video');
-      setInterval(handle, 1000);
-      PictureInPictureButton();
+
+  function waitForVideoPageLoad() {
+    if (location.pathname === "/watch" && document.readyState === "complete") {
+      video = document.querySelector('video.html5-main-video');
+      if (video) {
+        setInterval(handle, 1000);
+        addPictureInPictureButton();
+      } else if (retry < MAX_RETRY) {
+        retry++;
+        setTimeout(waitForVideoPageLoad, 1000);
+      }
     } else {
-      setTimeout(waitForPageLoad, 1000);
+      setTimeout(waitForVideoPageLoad, 1000);
     }
   }
-  waitForPageLoad();
+  waitForVideoPageLoad();
 }
