9dd5e487b5bf794ccb9f3ac41e4b7981f92b8ada
sucks555 <102584033+sucks-555@users.noreply.github.com>
2023-11-17 08:36:49 +0900

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -3,9 +3,8 @@ if (location.host === "www.youtube.com") {
   let permit = true;
   let retry = 0;
   const MAX_RETRY = 5;
-  const ytp = '.ytp-ad-simple-ad-badge, .ytp-ad-text,.ytp-ad-button,.ytp-ad-visit-advertiser-button,.ytp-ad-button-link,.ytp-ad-skip-button,.ytp-ad-overlay-close-button, .ytp-ad-preview-text-modern, .ytp-ad-preview-container .ytp-ad-preview-container-detached';
   function handle() {
-    const content = document.querySelectorAll(ytp);
+    const content = document.querySelectorAll("[class^='ytp-ad-text']");
     const firstContent = content[0] || false;
     if (firstContent) {
       const selector = document.querySelectorAll("[id^='skip-button']");
@@ -29,7 +28,7 @@ if (location.host === "www.youtube.com") {
     if (controls) {
       const button = document.createElement('button');
       button.innerHTML = 'PiP';
-      button.style.cssText = "height:55%;opacity:0.9;display:inline-block;width:48px;padding:0px 2px;overflow:hidden;position:relative;top:-20px;background:transparent;border:1px solid #fff;color:#fff;border-radius:100vh;";
+      button.style.cssText = "height:55%;opacity:0.9;display:inline-block;width:48px;padding:0px 2px;overflow:hidden;position:relative;top:-20px;background:transparent;border:1px solid #fff;color:#fff;border-radius:10vh;";
       button.addEventListener('click', togglePiP);
       controls.appendChild(button);
     } else if (retry < MAX_RETRY) {
@@ -44,7 +43,6 @@ if (location.host === "www.youtube.com") {
         ((retry < MAX_RETRY) ? (retry++, setTimeout(waitVideoLoad, 500)) : null) :
       setTimeout(waitVideoLoad, 1500);
   }
-
   waitVideoLoad();
   window.addEventListener('keydown', input);
   window.addEventListener('beforeunload', () => {window.removeEventListener('keydown', input);});
