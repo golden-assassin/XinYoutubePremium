8604a5554d73241565b64d4c576cda9204a2ae84
sucks555 <102584033+sucks-555@users.noreply.github.com>
2023-11-23 19:49:42 +0900

    Update blocker.js

--- scripts/blocker.js
+++ scripts/blocker.js
@@ -4,7 +4,7 @@ if (location.host === "www.youtube.com") {
     if (location.pathname === "/watch") {
       const button = document.querySelector("[id^='skip-button'] button");
       const content = document.querySelectorAll("[class^='ytp-ad-text']");
-      if (content || button) {
+      if (content[0] || button) {
         video = video || document.querySelector('video.html5-main-video');
         video ? (video.currentTime = video.duration || 999) : null;
         button && button.click();
