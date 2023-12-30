f29a90833845aed39143a828d357570e4c840c0a
sucks555 <102584033+sucks-555@users.noreply.github.com>
2023-12-30 20:03:00 +0900

    Update contents.js

--- scripts/contents.js
+++ scripts/contents.js
@@ -23,9 +23,8 @@ if (location.host === "www.youtube.com") {
       setTimeout(addPiPButton, 1000);
     }
   }
-
   function sponsor_ads() {
-    const sponsor_list = document.querySelectorAll("ytd-player-legacy-desktop-watch-ads-renderer, #player-ads")
+    const sponsor_list = document.querySelectorAll("ytd-player-legacy-desktop-watch-ads-renderer, #player-ads,ytd-video-masthead-ad-v3-renderer, ytd-video-masthead-ad-v3-renderer")
     for (const sponsor of sponsor_list) {
       sponsor && sponsor.remove()
     }
