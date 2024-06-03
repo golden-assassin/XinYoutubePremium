055bf7030bc2baf81c71c90e90160ec42b08a453
xin <102584033+golden-assassin@users.noreply.github.com>
2024-06-03 10:35:38 +0900

    Update blocker.js 警告無視してる

--- scripts/blocker.js
+++ scripts/blocker.js
@@ -2,14 +2,34 @@ if (location.host === "www.youtube.com") {
   var video;
   const handle = () => {
     if (location.pathname === "/watch") {
-      const button = document.querySelector("[id^='skip-button'] button");
-      const content = document.querySelectorAll("[class^='ytp-ad-text']");
-      if (content[0] || button) {
+      const button = document.querySelector("[id^='skip-button'] button, [class='ytp-skip-ad-button']");
+      const text = document.querySelector("[class^='ytp-ad-text']");
+      if (text || button) {
         video = video || document.querySelector('video.html5-main-video');
-        video ? (video.currentTime = video.duration || 999) : null;
+        video.muted = true;
+        video.playbackRate = 16;
         button && button.click();
-      }
+      } else {
+        if (video && video.playbackRate == 16) {
+          video.playbackRate = 1;
+          video.muted = false;
+          if (video.paused) {
+            video.play();
+          }
+        }
+      };
+      const styleId = '114514yaju';
+      const createStyleElement = () => {
+        const style = document.createElement('style');
+        style.id = styleId;
+        style.innerHTML = 'tp-yt-paper-dialog[modern] {overflow: auto !important;}tp-yt-paper-dialog , tp-yt-iron-overlay-backdrop {display: none !important;overflow: auto !important;}';
+        document.body.appendChild(style);
+      };
+      const element = document.getElementById(styleId);
+      !element && createStyleElement();
     } else video = null;
-  }
-  setInterval(handle, 800);
-}
+  };
+  setInterval(handle, 500);
+};
+
+// video ? (video.currentTime = video.duration || 999) : null;
